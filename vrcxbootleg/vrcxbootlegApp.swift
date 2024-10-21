9869e6a47061865dde19798731e710e63eae120c
Dar <heidar.neil342@gmail.com>
2024-10-21 09:55:09 +0700

    Initial Commit

---
+++ vrcxbootleg/vrcxbootlegApp.swift
@@ -0,0 +1,17 @@
+//
+//  vrcxbootlegApp.swift
+//  vrcxbootleg
+//
+//  Created by Dar on 21/10/24.
+//
+
+import SwiftUI
+
+@main
+struct vrcxbootlegApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
